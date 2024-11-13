2544c96294e16efb47d9765a3ff488cdeb70292c
nicol <nicolas.mahn@gmx.de>
2024-11-13 15:35:55 +0100

    added CORS

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask_graphql import GraphQLView
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 from config import GRAPHQL_SERVER_PORT
 from graph.schema import schema
 
 
 app = Flask(__name__)
+
+CORS(app, origins=["http://localhost:3000", "https://your-frontend-domain.com"])
 
 app.add_url_rule(
     '/graphql',

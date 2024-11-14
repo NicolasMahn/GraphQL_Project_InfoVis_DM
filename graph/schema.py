5d61c9f6f7c5c5b91755321c9e2ca07b6ebf09e2
nicol <nicolas.mahn@gmx.de>
2024-11-14 12:59:03 +0100

    added numb purchases per location

--- graph/schema.py
+++ graph/schema.py
@@ -1,11 +1,25 @@
 from graphene import ObjectType, Field, List, Schema, String
-from graph.types import PersonType
+from graph.types import PersonType, NumbPurchasesPerLocation
 from db.mongo import find_one, find_all
 
 class Query(ObjectType):
+    numb_purchases_per_location = List(NumbPurchasesPerLocation)
+
+    # Test Data
     person = Field(PersonType, name=String(required=True))
     people = List(PersonType)
 
+    def resolve_numb_purchases_per_location(self, info):
+        purchases_data = find_all("num_purchases_per_location")
+        return [
+            NumbPurchasesPerLocation(
+                location=purchase["location"],
+                numb_purchases_cc=purchase["numb_purchases_cc"],
+                numb_purchases_loyalty=purchase["numb_purchases_loyalty"]
+            ) for purchase in purchases_data
+        ]
+
+    # Test Data
     def resolve_person(self, info, name):
         person_data = find_one("test", {"name": name})
         if person_data:
@@ -15,5 +29,7 @@ class Query(ObjectType):
     def resolve_people(self, info):
         people_data = find_all("test")
         return [PersonType(name=person["name"], age=person["age"]) for person in people_data]
+
+
 
 schema = Schema(query=Query)
--- graph/types.py
+++ graph/types.py
@@ -3,3 +3,8 @@ from graphene import ObjectType, String, Int
 class PersonType(ObjectType):
     name = String()
     age = Int()
+
+class NumbPurchasesPerLocation(ObjectType):
+    location = String()
+    numb_purchases_cc = Int()
+    numb_purchases_loyalty = Int()

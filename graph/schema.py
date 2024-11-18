3e48475d6aa1d696dd199486ad89b8e20f7eafc6
nicol <nicolas.mahn@gmx.de>
2024-11-18 10:44:21 +0100

    added cleaned and sketchy card data

--- graph/schema.py
+++ graph/schema.py
@@ -1,9 +1,10 @@
 from graphene import ObjectType, Field, List, Schema, String
-from graph.types import PersonType, NumbPurchasesPerLocation
+from graph.types import *
 from db.mongo import find_one, find_all, find
 
 class Query(ObjectType):
     numb_purchases_per_location = List(NumbPurchasesPerLocation, locations=List(String))
+    location_comparison_cleaned_vs_sketchy = List(LocationComparisonCleanedVsSketchy, location=list(String))
 
     # Test Data
     person = Field(PersonType, name=String(required=True))
@@ -19,6 +20,24 @@ class Query(ObjectType):
                 location=purchase["location"],
                 numb_purchases_cc=purchase["numb_purchases_cc"],
                 numb_purchases_loyalty=purchase["numb_purchases_loyalty"]
+            ) for purchase in purchases_data
+        ]
+
+    def resolve_location_comparison_cleaned_vs_sketchy(self, info, locations=None):
+        if locations:
+            purchases_data = find("location_comparison_cleaned_vs_sketchy",
+                                  {"location": {"$in": locations}})
+        else:
+            purchases_data = find_all("location_comparison_cleaned_vs_sketchy")
+        return [
+            LocationComparisonCleanedVsSketchy(
+                location=purchase["location"],
+                absolut_cleaned=purchase["absolut_cleaned"],
+                absolut_sketchy=purchase["absolut_sketchy"],
+                percent_cleaned=purchase["percent_cleaned"],
+                percent_sketchy=purchase["percent_sketchy"],
+                avg_amount_cleaned=purchase["avg_amount_cleaned"],
+                avg_amount_sketchy=purchase["avg_amount_sketchy"]
             ) for purchase in purchases_data
         ]
 
--- graph/types.py
+++ graph/types.py
@@ -1,4 +1,5 @@
-from graphene import ObjectType, String, Int
+from graphene import ObjectType, String, Int, Float
+
 
 class PersonType(ObjectType):
     name = String()
@@ -8,3 +9,12 @@ class NumbPurchasesPerLocation(ObjectType):
     location = String()
     numb_purchases_cc = Int()
     numb_purchases_loyalty = Int()
+
+class LocationComparisonCleanedVsSketchy(ObjectType):
+    location = String()
+    absolut_cleaned = Int()
+    absolut_sketchy = Int()
+    percent_cleaned = Float()
+    percent_sketchy = Float()
+    avg_amount_cleaned = Float()
+    avg_amount_sketchy = Float()

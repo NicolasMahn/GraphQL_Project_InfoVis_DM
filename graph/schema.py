42fff04fa09c2c7e243565f87432149b9913a8e5
nicol <nicolas.mahn@gmx.de>
2024-11-19 15:33:41 +0100

    forgot to add the query

--- graph/schema.py
+++ graph/schema.py
@@ -6,6 +6,7 @@ from db.mongo import find_one, find_all, find
 class Query(ObjectType):
     numb_purchases_per_location = List(NumbPurchasesPerLocation, locations=List(String))
     comparing_purchases_of_pairs = List(ComparingPurchasesOfPairs, locations=List(String))
+    purchases_over_time = List(PurchasesOverTime, starttime=Float, endtime=Float)
 
     # Test Data
     person = Field(PersonType, name=String(required=True))
